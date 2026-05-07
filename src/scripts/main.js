27217a2fccd4b4a3f6f18535d0663aee1c58b75c
pavlo <nadazasada@gmail.com>
2026-05-07 18:19:32 +0300

    prepare

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,75 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const tree = document.createElement('ul');
+
+  // tree.textContent = 'Hello World!';
+
+  for (const key in data) {
+    const firstList = document.createElement('li');
+
+    firstList.textContent = key;
+
+    tree.append(firstList);
+    document.body.append(tree);
+
+    const test = Object.keys(data[key]).length;
+
+    let nested = data[key];
+
+    if (test !== 0) {
+      const next = createTree(null, data[key]);
+      firstList.append(next)
+    }
+  }
+
+  return tree;
 }
 
 createTree(tree, food);
+
+// console.log(food);
+// console.log(food.Drink);
+
+
+
+// ====
+
+    // function next() {
+    //   // console.log(data[key]);
+    //   for (const el in data[key]) {
+    //     // console.log(el);
+
+    //     const secondList = document.createElement('ul');
+    //     firstList.append(secondList);
+
+    //     const valueSeconList = document.createElement('li');
+    //     secondList.append(valueSeconList);
+
+    //     valueSeconList.textContent = el;
+    //   }
+
+    //   // firstList.append(secondList);
+    //   // secondList.append(valueSeconList);
+    // }
+
+    // if (test !== 0) {
+    //   // console.log(data[key]);
+    //   next(nested);
+    // }
+
+    // ====
+
+    // Move function declaration to function body root
+
+    // for (const el in data[key]) {
+    //   console.log(el);
+
+    //   const secondList = document.createElement('ul');
+    //   firstList.append(secondList);
+
+    //   const valueSeconList = document.createElement('li');
+    //   secondList.append(valueSeconList);
+
+    //   valueSeconList.textContent = el;
+    // }
