3549b95cd5d51e6141206f9ae78c86bd79c78a39
pavlo <nadazasada@gmail.com>
2026-05-08 14:34:33 +0300

    restart

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,12 +26,12 @@ function createTree(element, data) {
 
     firstList.textContent = key;
 
-    element.append(firstList);
-    // document.body.append(firstList);
+    // element.append(firstList);
+    document.body.append(firstList);
 
     const testLength = Object.keys(data[key]).length;
 
-    console.log(testLength);
+    // console.log(testLength);
 
     if (testLength > 0) {
       const ul = document.createElement('ul');
@@ -44,3 +44,66 @@ function createTree(element, data) {
 
   return element;
 }
+
+// if (tree) {
+//   createTree(tree, food);
+// }
+
+createTree(tree, food);
+
+// ====
+
+// 'use strict';
+
+// const food = {
+//   Drink: {
+//     Wine: {},
+//     Schnaps: {},
+//   },
+//   Fruit: {
+//     Red: {
+//       Cherry: {},
+//       Strawberry: {},
+//     },
+//     Yellow: {
+//       Banana: {},
+//       Pineapple: {},
+//     },
+//   },
+// };
+
+// const tree = document.querySelector('#tree');
+
+// function createTree(element, data) {
+//   // Твій основний цикл, який ти писав спочатку
+//   for (const key in data) {
+//     const firstList = document.createElement('li');
+
+//     firstList.textContent = key;
+
+//     // Додаємо пункт у поточний список (element)
+//     element.append(firstList);
+
+//     // Перевіряємо, чи є вкладені елементи
+//     const test = Object.keys(data[key]).length;
+
+//     if (test > 0) {
+//       // Якщо є вкладеність, створюємо НОВИЙ список для цього рівня
+//       const nestedUl = document.createElement('ul');
+
+//       // Додаємо цей список всередину нашого li
+//       firstList.append(nestedUl);
+
+//       // Викликаємо функцію знову для вкладеного списку
+//       // Це і є та сама рекурсія, яка замінює твій цикл "next"
+//       createTree(nestedUl, data[key]);
+//     }
+//   }
+// }
+
+// // Запускаємо один раз для головного контейнера
+// // if (tree) {
+// //   createTree(tree, food);
+// // }
+
+// createTree(tree, food);
